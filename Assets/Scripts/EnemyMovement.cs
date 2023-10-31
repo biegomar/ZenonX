7f31f9bae57610c5d1b389bc830ef26b87cfed97
Marc Biegota <marc.biegota@web.de>
2023-10-31 21:33:02 +0100

    add some enemies with some sinus movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private const int sinusMultiplier = 2;
+    private double activeSinus;
+    private float initialX;    
+
+    void Start()
+    {
+        this.activeSinus = 0d;
+        this.initialX = transform.position.x;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(
+               CalculateNewXPosition(),
+               CalculateNewYPosition(),
+               transform.position.z);
+    }
+
+    private float CalculateNewXPosition()
+    {
+        if (transform.position.y < 5) 
+        {
+            var result = this.initialX + (float)Math.Sin(activeSinus) * GameManager.EnemySinusAmplitude;
+
+            if (activeSinus < Math.PI * sinusMultiplier)
+            {
+                activeSinus += GameManager.EnemySinusStep;
+            }
+            else
+            {
+                activeSinus = 0d;
+            }
+
+            return result;
+        }
+
+        return transform.position.x;        
+    }
+
+    private float CalculateNewYPosition()
+    {        
+        return transform.position.y - GameManager.EnemyYStep * GameManager.EnemyYSpeed;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager
+{
+    public static float EnemySinusStep = 0.01f;
+    public static float EnemySinusAmplitude = 1.5f;
+    public static float EnemyYStep = 0.03f;
+    public static float EnemyYSpeed = 0.1f;
+}
