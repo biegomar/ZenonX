ed034056293bac08aad1277d002778f0ad8860f9
Marc Biegota <marc.biegota@web.de>
2023-11-01 17:22:50 +0100

    introduce laser power

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,4 +10,6 @@ public static class GameManager
     public static float EnemyYSpeed = 0.1f;
 
     public static float ShipLaserSpeed = 5f;
+    public static uint MaxShipLaserPower = 100;
+    public static float LaserPowerRegainFactor = 0.3f;
 }
--- Assets/Scripts/SpaceShip/FireControl.cs
+++ Assets/Scripts/SpaceShip/FireControl.cs
@@ -5,17 +5,36 @@ using UnityEngine;
 public class FireControl : MonoBehaviour
 {
     [SerializeField]
-    private GameObject Laser;    
+    private GameObject Laser;
 
-    // Update is called once per frame
-    void Update()
+    private uint ActualLaserPower;
+
+    public void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        this.ActualLaserPower = GameManager.MaxShipLaserPower;
+        InvokeRepeating(nameof(GainFirePower), 1f, GameManager.LaserPowerRegainFactor);
+    }
+
+    public void Update()
+    {
+        if (this.ActualLaserPower > 0 && Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(Laser, new Vector3(
                transform.position.x,
                transform.position.y + 0.3f,
                transform.position.z), Quaternion.identity);
+
+            this.ActualLaserPower--;
         }
+
+        Debug.Log($"FirePower: {this.ActualLaserPower}");
+    }
+
+    private void GainFirePower()
+    {
+        if (this.ActualLaserPower < 100)
+        {
+            this.ActualLaserPower++;
+        }               
     }
 }
