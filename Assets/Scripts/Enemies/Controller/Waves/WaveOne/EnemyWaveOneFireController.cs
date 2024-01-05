ccbe9fa1fde7a7571850f572a9b4665bfbcdac68
Marc Biegota <marc.biegota@web.de>
2024-01-05 21:02:09 +0100

    add enemy laser sound

--- Assets/Scripts/Enemies/Controller/Waves/WaveOne/EnemyWaveOneFireController.cs
+++ Assets/Scripts/Enemies/Controller/Waves/WaveOne/EnemyWaveOneFireController.cs
@@ -9,34 +9,17 @@ public class EnemyWaveOneFireController : MonoBehaviour
     [SerializeField]
     private GameObject Laser;
 
-    private EnemyWaveOneSpawnController enemyController;
-    private EnemyItem enemyItem;
+    [SerializeField]
+    private AudioSource laserSound;
+    
     private float fireInterval;
     private float elapsedTimeSinceLastShot;
 
     void Start()
     {
         this.fireInterval = Random.Range(2f, 6f);
-
         this.elapsedTimeSinceLastShot = this.fireInterval - (this.fireInterval * .5f);
-
-        GameObject go = GameObject.Find("EnemyWaveOne");
-        if (go != null)
-        {
-            this.enemyController = go.GetComponent<EnemyWaveOneSpawnController>();
-            if (this.enemyController != null)
-            {
-                this.enemyItem = this.enemyController.Enemies[gameObject.GetInstanceID()];
-            }
-            else
-            {
-                Debug.Log("EnemyWaveOneFireController.GetComponent<EnemyController>() is null");
-            }
-        }
-        else
-        {
-            Debug.Log("EnemyWaveOneFireController.Find(Enemies) is null");
-        }
+        this.laserSound.enabled = true;
     }
 
     private void Update()
@@ -52,6 +35,8 @@ public class EnemyWaveOneFireController : MonoBehaviour
     {
         if (this.elapsedTimeSinceLastShot >= this.fireInterval)
         {
+            this.laserSound.Play();
+
             Instantiate(Laser, new Vector3(
                transform.position.x,
                transform.position.y - 0.3f,
