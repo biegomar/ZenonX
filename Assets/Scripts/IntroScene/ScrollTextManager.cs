7006f21d4854b693fe6fcee70a0fef8039e2d1cf
Marc Biegota <marc.biegota@web.de>
2024-02-18 20:36:31 +0100

    simple text scroller

---
+++ Assets/Scripts/IntroScene/ScrollTextManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ScrollTextManager : MonoBehaviour
+{
+    [SerializeField] private Vector3 startPosition;
+    [SerializeField] private Vector3 endPosition;
+    [SerializeField] private bool looping;
+    [SerializeField] private float scrollSpeed;
+    [SerializeField] private RectTransform scrollText;
+
+    private RectTransform textTransform;
+
+    private void Start()
+    {
+        this.scrollText.localPosition = this.startPosition;
+    }
+
+    private void Update()
+    {
+        if (this.scrollText.localPosition.x > this.endPosition.x)
+        {
+            
+            this.scrollText.Translate(Vector3.left * (this.scrollSpeed * Time.deltaTime));
+        }
+        else
+        {
+            this.scrollText.localPosition = this.startPosition;
+        }
+    }
+}
